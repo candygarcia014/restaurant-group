5c88c8fa575b97336bbeb6b8a4eabdbfff352042
JohnWestermeyer <johnwestermeyer@gmail.com>
2020-12-01 10:47:06 -0800

    adding server.js and gitignore

---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.port || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let tables, reserves, waitList = [];
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/api/tables", function(req, res) {
+    return res.json(reserves);
+  });
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitList);
+  });
+
+app.get("/", function(req, res) {
+    var chosen = req.params.character;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === characters[i].routeName) {
+        return res.json(characters[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+app.post("/api/characters", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newCharacter = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newCharacter);
+  
+    characters.push(newCharacter);
+  
+    res.json(newCharacter);
+  });
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
